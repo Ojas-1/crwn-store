e50dd333308d48890abe17765be9060c7595a648
Ojas Malghan <ojas.malghan7@gmail.com>
2023-06-27 07:10:26 -0400

    files

---
+++ src/App.js
@@ -0,0 +1,41 @@
+const App = () => {
+  const categories = [
+    {
+      id: 0,
+      title: "Hats",
+    },
+    {
+      id: 1,
+      title: "Jackets",
+    },
+    {
+      id: 2,
+      title: "Sneakers",
+    },
+    {
+      id: 3,
+      title: "Womens",
+    },
+
+    {
+      id: 4,
+      title: "Mens",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          {/* image */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p> Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
